170c6acc033760a2d9db852a88ac2c432677796a
gagfe801 <94819677+gagfe801@users.noreply.github.com>
2021-11-21 23:17:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,11 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
- apiKey: "AIzaSyBf6iAXoudeX-QH2OPXQrmhbVXyrCRv0N0",
-  authDomain: "chat801cls-cb7c5.firebaseapp.com",
-  projectId: "chat801cls-cb7c5",
-  storageBucket: "chat801cls-cb7c5.appspot.com",
-  messagingSenderId: "167329011743",
-  appId: "1:167329011743:web:9b1e74f3006bda54d81d2a",
-  measurementId: "G-RNDPH4GCQE"
-    });
+const firebaseConfig = {
+ apiKey: "AIzaSyBMRIiRMPF5RilmVMt83SPxSLhaqTg2FyA",
+  authDomain: "chat801fgg-8cfdc.firebaseapp.com",
+  projectId: "chat801fgg-8cfdc",
+  storageBucket: "chat801fgg-8cfdc.appspot.com",
+  messagingSenderId: "389412464063",
+  appId: "1:389412464063:web:11f779543fde3126466d0a"
+};
