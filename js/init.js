d29228d58e0612d736c6b11d0fab90acf5cffcdf
gagfe801 <94819677+gagfe801@users.noreply.github.com>
2021-11-21 23:01:59 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyBf6iAXoudeX-QH2OPXQrmhbVXyrCRv0N0",
+  authDomain: "chat801cls-cb7c5.firebaseapp.com",
+  projectId: "chat801cls-cb7c5",
+  storageBucket: "chat801cls-cb7c5.appspot.com",
+  messagingSenderId: "167329011743",
+  appId: "1:167329011743:web:9b1e74f3006bda54d81d2a",
+  measurementId: "G-RNDPH4GCQE"
+    });
