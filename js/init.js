37860739135ce16efbdf96088e5ed9639d67486a
chat801fgg <94819677+chat801fgg@users.noreply.github.com>
2021-11-21 23:24:20 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-const firebaseConfig = {
+firebase.initializeApp({
  apiKey: "AIzaSyBMRIiRMPF5RilmVMt83SPxSLhaqTg2FyA",
   authDomain: "chat801fgg-8cfdc.firebaseapp.com",
   projectId: "chat801fgg-8cfdc",
   storageBucket: "chat801fgg-8cfdc.appspot.com",
   messagingSenderId: "389412464063",
   appId: "1:389412464063:web:11f779543fde3126466d0a"
-};
+});
